a08b61b57938a0bcb0926b52a07e58b73a87cd76
Boualem234 <2534056@etu.cchic.ca>
2025-09-16 14:43:03 -0400

    CRUD base OK

--- app/urls.py
+++ app/urls.py
@@ -5,6 +5,18 @@ from . import views
 urlpatterns = [
     path('', views.IndexView.as_view(), name="index"),
     path('books/', views.BookListView.as_view(), name="book_list"),
+    path('categories/', views.CategoryListView.as_view(), name="cat_list"),
+    path('authors/', views.AuthorListView.as_view(), name="author_list"),
     path('bookDetails/<int:pk>/', views.BookDetailsView.as_view(), name="book_details"),
+    path('catDetails/<int:pk>/', views.CategoryDetailsView.as_view(), name="cat_details"),
+    path('authorDetails/<int:pk>/', views.AuthorDetailsView.as_view(), name="author_details"),
     path('addBook/', views.AddBookView.as_view(), name="add_book"),
+    path('addCategory/', views.AddCategoryView.as_view(), name="add_category"),
+    path('addAuthor/', views.AddAuthorView.as_view(), name="add_author"),
+    path('delBook/<int:pk>/', views.DelBookView.as_view(), name="del_book"),
+    path('delCategory/<int:pk>/', views.DelCategoryView.as_view(), name="del_cat"),
+    path('delAuthor/<int:pk>/', views.DelAuthorView.as_view(), name="del_author"),
+    path('editBook/<int:pk>/', views.EditBookView.as_view(), name="edit_book"),
+    path('editCategory/<int:pk>/', views.EditCategoryView.as_view(), name="edit_cat"),
+    path('editAuthor/<int:pk>/', views.EditAuthorView.as_view(), name="edit_author"),
 ]
--- app/views.py
+++ app/views.py
@@ -51,3 +51,164 @@ class BookDetailsView(View):
         except Book.DoesNotExist:
             raise Http404("Livre non trouvé")
         return render(request, 'book_details.html', {'book': book})
+    
+class EditBookView(View):
+    def get(self, request, pk):
+        try:
+            book = Book.objects.get(pk=pk)
+        except Book.DoesNotExist:
+            raise Http404("Livre non trouvé")
+        categories = Category.objects.all()
+        authors = Author.objects.all()
+        return render(request, 'edit_book.html', {'book': book, 'categories': categories, 'authors': authors})
+    
+    def post(self, request, pk):
+        try:
+            book = Book.objects.get(pk=pk)
+        except Book.DoesNotExist:
+            raise Http404("Livre non trouvé")
+
+        book.code = request.POST.get('code')
+        book.title = request.POST.get('title')
+        book.price = request.POST.get('price')
+        book.summary = request.POST.get('summary')
+        book.date = request.POST.get('date')
+        book.edition = request.POST.get('edition')
+        
+        category_id = request.POST.get('category')
+        author_id = request.POST.get('author')
+
+        book.category = Category.objects.get(id=category_id)
+        book.author = Author.objects.get(id=author_id)
+
+        book.save()
+        return render(request, 'edit_book.html', {'book': book, 'success_message': "Livre mis à jour avec succès!"})
+    
+class DelBookView(View):
+    def get(self, request, pk):
+        try:
+            book = Book.objects.get(pk=pk)
+            book.delete()
+            books = Book.objects.all()
+            return render(request, 'book_list.html', {'books': books, 'success_message': "Livre supprimé avec succès!"})
+        except Book.DoesNotExist:
+            raise Http404("Livre non trouvé")
+        
+class CategoryListView(View):
+    def get(self, request):
+        categories = Category.objects.all()  # Récupère toutes les catégories
+        return render(request, 'cat_list.html', {'categories': categories})
+    
+class CategoryDetailsView(View):
+    def get(self, request, pk):
+        try:
+            category = Category.objects.get(pk=pk)
+        except Category.DoesNotExist:
+            raise Http404("Catégorie non trouvée")
+        return render(request, 'cat_details.html', {'category': category})
+    
+class AddCategoryView(View):
+    def get(self, request):
+        return render(request, 'add_category.html')
+    
+    def post(self, request):
+        name = request.POST.get('name')
+
+        category = Category(
+            name=name,
+        )
+        category.save()
+        return render(request, 'add_category.html', {'success_message': "Catégorie ajoutée avec succès!"})
+    
+class DelCategoryView(View):
+    def get(self, request, pk):
+        try:
+            category = Category.objects.get(pk=pk)
+            category.delete()
+            categories = Category.objects.all()
+            return render(request, 'cat_list.html', {'categories': categories, 'success_message': "Catégorie supprimée avec succès!"})
+        except Category.DoesNotExist:
+            raise Http404("Catégorie non trouvée")
+        
+class EditCategoryView(View):
+    def get(self, request, pk):
+        try:
+            category = Category.objects.get(pk=pk)
+        except Category.DoesNotExist:
+            raise Http404("Catégorie non trouvée")
+        return render(request, 'edit_category.html', {'category': category})
+    
+    def post(self, request, pk):
+        try:
+            category = Category.objects.get(pk=pk)
+        except Category.DoesNotExist:
+            raise Http404("Catégorie non trouvée")
+
+        category.name = request.POST.get('name')
+        category.description = request.POST.get('description')
+        category.save()
+        return render(request, 'edit_category.html', {'category': category, 'success_message': "Catégorie mise à jour avec succès!"})
+    
+class AuthorListView(View):
+    def get(self, request):
+        authors = Author.objects.all()  # Récupère tous les auteurs
+        return render(request, 'author_list.html', {'authors': authors})
+    
+class AuthorDetailsView(View):
+    def get(self, request, pk):
+        try:
+            author = Author.objects.get(pk=pk)
+        except Author.DoesNotExist:
+            raise Http404("Auteur non trouvé")
+        return render(request, 'author_details.html', {'author': author})
+    
+class AddAuthorView(View):
+    def get(self, request):
+        return render(request, 'add_author.html')
+    
+    def post(self, request):
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        birth_date = request.POST.get('birth_date')
+        nationality = request.POST.get('nationality')
+
+        author = Author(
+            first_name=first_name,
+            last_name=last_name,
+            birth_date=birth_date,
+            nationality=nationality)
+        
+        author.save()
+        return render(request, 'add_author.html', {'success_message': "Auteur ajouté avec succès!"})
+    
+class DelAuthorView(View):
+    def get(self, request, pk):
+        try:
+            author = Author.objects.get(pk=pk)
+            author.delete()
+            authors = Author.objects.all()
+            return render(request, 'author_list.html', {'authors': authors, 'success_message': "Auteur supprimé avec succès!"})
+        except Author.DoesNotExist:
+            raise Http404("Auteur non trouvé")
+        
+class EditAuthorView(View):
+        def get(self, request, pk):
+            try:
+                author = Author.objects.get(pk=pk)
+            except Category.DoesNotExist:
+                raise Http404("Catégorie non trouvée")
+            return render(request, 'edit_author.html', {'author': author})
+
+        def post(self, request, pk):
+            try:
+                author = Author.objects.get(pk=pk)
+            except Category.DoesNotExist:
+                raise Http404("Auteur non trouvée")
+
+            author.first_name = request.POST.get('first_name')
+            author.last_name = request.POST.get('last_name')
+            author.birth_date = request.POST.get('birth_date')
+            author.nationality = request.POST.get('nationality')
+            author.save()
+            return render(request, 'edit_author.html', {'author': author, 'success_message': "Auteur mis à jour avec succès!"})
+    
